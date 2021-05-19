d422f93ac5b39b3aeb912033e60bc45cf08faa44
Gustavo Seabra <devguga87@gmail.com>
2021-05-19 10:28:00 -0300

    feature: creating articles and search

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export const Button = ({text, onClick, disabled}) => {
+  return (
+    <button
+      className='button' 
+      disabled={disabled} 
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  )
+}
