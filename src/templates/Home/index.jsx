b388dbd2129fac3574d161d755cf8abcaa392bea
Gustavo Seabra <devguga87@gmail.com>
2021-06-04 10:13:04 -0300

    feature:useFetch

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,72 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-import './styles.css';
-
-import { Posts } from '../../components/Posts';
-import { loadPosts } from '../../utils/loadPosts';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
-
-export const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(10);
-  const [searchValue, setSearchValue] = useState('');
-
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-
-  const filteredPosts = searchValue
-    ? allPosts.filter((post) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-    : posts;
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    handleLoadPosts();
-  }, []);
+    setLoading(true);
 
-  const handleLoadPosts = async () => {
-    const postsAndPhotos = await loadPosts();
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 3000));
+      try {
+        const response = await fetch(url, options);
+        const jsonResult = await response.json();
+        setResult(jsonResult);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        throw e;
+      }
+    };
 
-    setPosts(postsAndPhotos.slice(page, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  };
+    fetchData();
+  }, [url, options]);
 
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    posts.push(...nextPosts);
+  return [result, loading];
+};
 
-    setPosts(posts);
-    setPage(nextPage);
-  };
+export const Home = () => {
+  const [result, loading] = useFetch('http://jsonplaceholder.typicode.com/posts');
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-  };
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-  return (
-    <section className="container">
-      <div className="search-container">
-        {searchValue && (
-          <>
-            <h1>Search value:{searchValue}</h1>
-            <br />
-          </>
-        )}
+  if (!loading && result) {
+    console.log(result);
+  }
 
-        <TextInput handleChange={handleChange} searchValue={searchValue} />
-      </div>
-
-      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-
-      {filteredPosts.length === 0 && <p>Não existem posts</p>}
-
-      <div className="button-container">
-        {!searchValue && <Button text="Load more posts" onClick={loadMorePosts} disabled={noMorePosts} />}
-      </div>
-    </section>
-  );
+  return <h1>Oi</h1>;
 };
